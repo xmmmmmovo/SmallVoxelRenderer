60b121bb06ca695dda6c50e8c8264637a6c953b3
xmmmmmovo <13256172360@163.com>
2023-06-30 22:33:21 +0100

    First Commit

---
+++ voxel-renderer/core/path.hpp
@@ -0,0 +1,10 @@
+#ifndef VOXEL_RENDERER_PATH_HPP_IN
+#define VOXEL_RENDERER_PATH_HPP_IN
+
+#ifndef NDEBUG
+#define DEBUG_PATH "/Users/xmmmmmovo/Documents/code/SmallVoxelRenderer"
+#else
+#define DEBUG_PATH ""
+#endif
+
+#endif
